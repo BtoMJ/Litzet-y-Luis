11c8df56836818d236e3b2cbdf0a73e2937c9d5f
BtoMJ <bto.mena.17@gmail.com>
2026-09-24 14:22:17 -0600

    Pantalla para posición landscape

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,25 @@
 import home from "../../assets/Home.png";
 import greca from "../../assets/greca.png";
+import ViewLandscape from "../ViewLandscape/ViewLandscape";
 import "./Home.css";
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-photo">
-        <img src={home} alt="foto de los novios" />
+    <>
+      <div className="home-container">
+        <div className="home-photo">
+          <img src={home} alt="foto de los novios" />
+        </div>
+        <div className="names-container">
+          <p className="names">
+            Litzet <span>&</span> Luis
+          </p>
+          <p className="date-names">07 Noviembre 2026</p>
+          <img src={greca} alt="adorno de la portada" />
+        </div>
       </div>
-      <div className="names-container">
-        <p className="names">
-          Litzet <span>&</span> Luis
-        </p>
-        <p className="date-names">07 Noviembre 2026</p>
-        <img src={greca} alt="adorno de la portada" />
-      </div>
-    </div>
+      <ViewLandscape />
+    </>
   );
 }
 
--- src/components/ViewLandscape/ViewLandscape.jsx
+++ src/components/ViewLandscape/ViewLandscape.jsx
@@ -1,18 +1,17 @@
-// import flores2 from "../../assets/flores2.png";
+import flores1 from "../../assets/flower-left.png";
+import flores2 from "../../assets/flower-rigth.png";
 import "./ViewLandscape.css";
 
 function ViewLandscape() {
   return (
     <section className="ViewLandscape-container">
-      <div className="name">
-        {/* <img className="flower2" src={flores2} alt="flores arriba" /> */}
-        <p className="names">
-          Litzet <span>&</span> Luis
-        </p>
-        <h4>MIS XV AÑOS</h4>
-      </div>
+      <img className="flower1" src={flores1} alt="flores arriba" />
+      <p className="names">
+        Litzet <span>&</span> Luis
+      </p>
+      <h4>Nuestra Boda</h4>
       <h5>Por favor rota tu dispositivo para una mejor experiencia</h5>
-      {/* <img className="flower1" src={flores2} alt="flores arriba" /> */}
+      <img className="flower2" src={flores2} alt="flores arriba" />
     </section>
   );
 }
